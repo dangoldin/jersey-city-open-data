0289284e4fdcaf77a1c25cb12bce8c94e3d3993a
Dan Goldin <dangoldin@gmail.com>
2015-09-14 18:31:01 -0700

    Got hull working

--- geocode.py
+++ geocode.py
@@ -1,4 +1,8 @@
 import json
+from geopy.geocoders import Nominatim
+from pyhull.convex_hull import ConvexHull
+
+geolocator = Nominatim()
 
 def get_addrs(addr_range):
     parts = addr_range.split('--')
@@ -12,32 +16,34 @@ def get_addrs(addr_range):
     return []
 
 def geocode(addr):
-    lat, lng = geopy.geocode(addr)
-    return (lat, lng)
+    loc = geolocator.geocode(addr)
+    return (loc.latitude, loc.longitude)
 
-# Figure out the algo here
-def convex_poly(lat_lngs):
-    poly = []
-    for lat, lng in lat_lngs:
-        pass
-    return poly
+def convex_hull(lat_lngs):
+    hull = ConvexHull(lat_lngs)
+    return hull.vertices
 
 with open('zone-permit-zones.json','r') as f:
     j = json.loads(f.read())
 
     out = {}
     for zone, zone_addrs in j.iteritems():
-        addrs = []
-        for addr_range in zone_addrs:
-            addr_pair = get_addrs(addr_range)
-            if len(addr_pair) > 0:
-                addrs.extend(addr_pair)
+        if zone == 'ZONE 1':
+            addrs = []
+            for addr_range in zone_addrs:
+                addr_pair = get_addrs(addr_range)
+                if len(addr_pair) > 0:
+                    addrs.extend(addr_pair)
 
-        print addrs
+            print addrs
 
-        lat_lngs = [geocode(a) for a in addrs]
+            lat_lngs = [geocode(a) for a in addrs]
 
-        out[zone] = convex_poly(lat_lngs)
+            print lat_lngs
+
+            out[zone] = convex_hull(lat_lngs)
+
+        print out
 
     with open('zone-permit-geocoded.json', 'w') as g:
         g.write(json.dumps(out, indent=2))
